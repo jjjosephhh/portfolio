206b2a6d77436c407a9980a57cfc1f26fcd960e5
Joseph Sungpil Choi <chousemath@gmail.com>
2022-10-24 04:49:22 +0900

    [INIT] first commit

---
+++ src/components/MenuMobile.svelte
@@ -0,0 +1,38 @@
+<script>
+  let open = false;
+  $: menuVisiblity = open ? "block" : "hidden";
+  $: menuButtonVisibility = open ? "hidden" : "block";
+  const links = [
+    { title: "home", href: "" },
+    { title: "about me", href: "" },
+    { title: "works", href: "" },
+    { title: "contact", href: "" },
+  ];
+  const toggleMenu = () => (open = !open);
+</script>
+
+<img
+  on:click={toggleMenu}
+  on:keydown={() => null}
+  class={`w-5 cursor-pointer ${menuButtonVisibility} md:hidden z-20`}
+  src="/images/icons8-menu-90.png"
+  alt="menu button"
+/>
+
+<img
+  on:click={toggleMenu}
+  on:keydown={() => null}
+  class={`w-5 cursor-pointer ${menuVisiblity} md:hidden z-20`}
+  src="/images/icons8-close-48.png"
+  alt="menu button"
+/>
+
+<ul
+  class={`${menuVisiblity} bg-indigo-900 absolute left-0 top-0 w-full p-10 rounded-b-3xl space-y-10 text-white text-center z-10`}
+>
+  {#each links as link}
+    <li>
+      <a href="#">{link.title}</a>
+    </li>
+  {/each}
+</ul>
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "480px",
+      },
+      spacing: {
+        big: "48rem",
+      },
+    },
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
